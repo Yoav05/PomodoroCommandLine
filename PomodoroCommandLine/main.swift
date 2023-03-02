58cdfb64b13c062c49ce2be78808d12cc83cc05b
Yoav <42143551+Yoav05@users.noreply.github.com>
2023-03-02 10:39:38 +0300

    Add command line arguments

--- PomodoroCommandLine/main.swift
+++ PomodoroCommandLine/main.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 func startTimer(interval: TimeInterval, completedIntervals: Int, totalIntervals: Int, restTime: TimeInterval) {
+    print()
     let pomodoroDuration = interval * 60.0 // interval in seconds
     var remainingTime = pomodoroDuration
     let timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
@@ -8,11 +9,11 @@ func startTimer(interval: TimeInterval, completedIntervals: Int, totalIntervals:
         let minutes = Int(remainingTime) / 60
         let seconds = Int(remainingTime) % 60
         let remainingTimeString = String(format: "%02d:%02d", minutes, seconds)
-        print("\rInterval \(completedIntervals + 1) of \(totalIntervals) - Time remaining: \(remainingTimeString)", terminator: "")
+        print("\rTime remaining: \(remainingTimeString)", terminator: "")
         fflush(stdout)
         if remainingTime <= 0 {
             timer.invalidate()
-            print("\rInterval \(completedIntervals + 1) of \(totalIntervals) - Pomodoro complete!")
+            print("\rInterval \(completedIntervals + 1)/\(totalIntervals) - complete!\n")
             if completedIntervals + 1 < totalIntervals {
                 startRestingTimer(interval: restTime) {
                     startTimer(
@@ -28,12 +29,12 @@ func startTimer(interval: TimeInterval, completedIntervals: Int, totalIntervals:
             }
         }
     }
-    print("Pomodoro timer started for \(interval) minutes with a total of \(totalIntervals) intervals and \(restTime) minutes of rest time!")
+    print("Interval \(completedIntervals + 1)/\(totalIntervals)")
     RunLoop.current.add(timer, forMode: .common)
 }
 
 func startRestingTimer(interval: TimeInterval, completion: @escaping () -> Void) {
-    print("Rest for \(interval) minutes. Press enter to start rest.", terminator: "")
+    print("\rPress enter to start rest.", terminator: "")
     let _ = readLine()
     let restingTime = interval * 60.0
     var remainingTime = restingTime
@@ -42,11 +43,11 @@ func startRestingTimer(interval: TimeInterval, completion: @escaping () -> Void)
         let minutes = Int(remainingTime) / 60
         let seconds = Int(remainingTime) % 60
         let remainingTimeString = String(format: "%02d:%02d", minutes, seconds)
-        print("\rResting - Time remaining: \(remainingTimeString)", terminator: "")
+        print("\rTime remaining: \(remainingTimeString)", terminator: "")
         fflush(stdout)
         if remainingTime <= 0 {
             timer.invalidate()
-            print("\nRest for \(interval) minutes. Press enter to start next interval.", terminator: "")
+            print("\rPress enter to end rest.", terminator: "")
             let _ = readLine()
             completion()
         }
@@ -54,20 +55,48 @@ func startRestingTimer(interval: TimeInterval, completion: @escaping () -> Void)
     RunLoop.current.add(timer, forMode: .common)
 }
 
-print("Enter the interval time in minutes:")
-if let intervalString = readLine(), let interval = Double(intervalString) {
-    print("Enter the total number of intervals:")
-    if let totalIntervalsString = readLine(), let totalIntervals = Int(totalIntervalsString) {
-        print("Enter the rest time in minutes:")
-        if let restTimeString = readLine(), let restTime = Double(restTimeString) {
-            startTimer(interval: interval, completedIntervals: 0, totalIntervals: totalIntervals, restTime: restTime)
-            RunLoop.current.run()
-        } else {
-            print("Invalid rest time")
+func parseArguments() -> (interval: Double, totalIntervals: Int, restTime: Double)? {
+    // Set default values
+    var interval = 0.1
+    var totalIntervals = 2
+    var restTime = 0.1
+    
+    // Get the command line arguments
+    let args = CommandLine.arguments
+    
+    // Parse the arguments
+    var i = 1
+    while i < args.count {
+        switch args[i] {
+        case "--interval", "-i":
+            if let value = Double(args[i+1]) {
+                interval = value
+            }
+            i += 2
+        case "--total-intervals", "-t":
+            if let value = Int(args[i+1]) {
+                totalIntervals = value
+            }
+            i += 2
+        case "--rest-time", "-r":
+            if let value = Double(args[i+1]) {
+                restTime = value
+            }
+            i += 2
+        default:
+            print("Unknown argument: \(args[i])")
+            return nil
         }
-    } else {
-        print("Invalid total number of intervals")
     }
-} else {
-    print("Invalid interval time")
+    
+    // Return the parsed arguments
+    return (interval, totalIntervals, restTime)
 }
+
+func main() {
+    guard let (interval, totalIntervals, restTime) = parseArguments() else { return }
+    startTimer(interval: interval, completedIntervals: 0, totalIntervals: totalIntervals, restTime: restTime)
+    RunLoop.current.run()
+}
+
+main()
